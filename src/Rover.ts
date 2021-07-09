91b0e43d4d4fe2cde5a706fb74626a3a1d53369e
Zak Smith <zsmith3.dev@gmail.com>
2021-07-09 09:55:05 +0100

    Add earth min/max dates to Rover

--- src/Rover.ts
+++ src/Rover.ts
@@ -4,12 +4,16 @@ export default class Rover {
     id: number;
     name: string;
     max_sol: number;
+    landing_date: string;
+    max_date: string;
     cameras: Camera[];
 
     constructor (rover: Rover) {
         this.id = rover.id;
         this.name = rover.name;
         this.max_sol = rover.max_sol;
+        this.landing_date = rover.landing_date;
+        this.max_date = rover.max_date;
         this.cameras = rover.cameras ? rover.cameras.map(camera => new Camera(camera)) : [];
     }
 }
